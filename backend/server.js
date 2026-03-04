6330aa711c17610f6a3ee8ea691321b523399055
Piyumal <pasindupiumalcrw2002@gmail.com>
2026-03-04 09:36:19 +0530

    update user function

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
+//routes import
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
